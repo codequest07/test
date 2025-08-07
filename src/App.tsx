97d9fbc7cdd784d4701afcba92a645001ec58edd
Samson Aderonmu <Superior212@users.noreply.github.com>
2025-08-07 09:40:18 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
+      <h1 className="text-6xl font-bold text-white">Hello, World!</h1>
+    </div>
+  )
+}
+
+export default App
